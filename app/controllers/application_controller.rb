8a5ca61088c1ae75a9534f4019f3cf6cc3473148
Ranika Williams <rnkprice07@gmail.com>
2019-09-19 17:38:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		user = User.new(params[:username], params[:password])
+		user =  user = User.new(:username => params[:username], :password => params[:password])
 		
 		if user.save
 		  redirect "/login"
